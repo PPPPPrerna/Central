f40f6b514bc473db9f181bb72eca4b027ff1479e
PPPPPrerna <pandeyprerna248@gmail.com>
2020-10-05 08:41:46 +0530

    good

---
+++ src/main/java/quick/Sm/App.java
@@ -0,0 +1,13 @@
+package quick.Sm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/quick/Sm/DateTest.java
@@ -0,0 +1,28 @@
+package quick.Sm;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DateTest {
+
+	@Test
+	public void display() {
+
+		int num =3;
+		
+		for(int i=1 ;i<num; i++) {
+			
+			num = num*i;
+		}
+		System.out.println(num);
+	
+	}
+			
+	}
+
